4bf0154bfbc344105ccd04573b084c39ba20526e
Yoshiki-Hidaka <y.hidaka.kobe@gmail.com>
2020-11-09 19:18:27 +0900

    modify .env file

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,6 +1,13 @@
 const contentful = require('contentful')
 
-const config = {
+// 開発環境時のみ下書きの記事を表示する
+const config = (process.env.NODE_ENV === 'development') ? {
+  // 開発環境時の処理
+  space: process.env.CTF_SPACE_ID,
+  accessToken: process.env.CTF_PREVIEW_ACCESS_TOKEN,
+  host: 'preview.contentful.com'
+} : {
+  // 本番環境時の処理
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_CDA_ACCESS_TOKEN
 }
