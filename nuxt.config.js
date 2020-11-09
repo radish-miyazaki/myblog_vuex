361df70c49c564c07c1dc3218f63ac60d2f045eb
Yoshiki-Hidaka <y.hidaka.kobe@gmail.com>
2020-11-09 16:05:10 +0900

    implement vuetify2

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -18,6 +20,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    'plugins/vuetify'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -25,7 +28,26 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    '@nuxtjs/vuetify'
   ],
+  vuetify: {
+    theme: {
+      themes: {
+        light: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+          twitter: '#55acee',
+          facebook: '#3b5998',
+          background: colors.grey.lighten5
+        }
+      }
+    }
+  },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
@@ -33,5 +55,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    transpile: ['vuetify/lib'],
+    extend(config, ctx) {}
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,12 @@
         <v-flex xs12 sm8 md6>
           <div class="text-xs-center">
             <h1>Hello Nuxt</h1>
+            <v-icon>fas fa-lock</v-icon>
+            <v-icon
+              color="twitter"
+            >
+              fab fa-twitter
+            </v-icon>
           </div>
         </v-flex>
       </v-layout>
